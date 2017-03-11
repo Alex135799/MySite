4b29da87cd0783e8618bec10cb71b89377f46fbc
Alex <ooglopcid@gmail.com>
2017-03-11 13:50:57 -0500

    fix chrome?

--- apps/app_social_stream/controllers/home.js
+++ apps/app_social_stream/controllers/home.js
@@ -19,7 +19,7 @@
 		var canFullscreen = Fullscreen.isSupported();	
 		var autoPlayWait = 3000;
 		setDisplayHeight(50);
-		$('#progBarContainer').css('padding-top',$('#progBarContainer').height()/2 - $('#progBar').height()/2);
+		//$('#progBarContainer').css('padding-top',$('#progBarContainer').height()/2 - $('#progBar').height()/2);
 		$(window).resize(function(){
 			setDisplayHeight()
 		})
